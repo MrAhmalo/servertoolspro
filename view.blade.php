c90029674d59a71cc9927bc7a34bb5f18be3f2fe
MrAhmalo <ahmalo456@gmail.com>
2024-12-05 22:28:26 +0100

    Initial Commit

---
+++ view.blade.php
@@ -0,0 +1,11 @@
+<div class="box box-info">
+  <div class="box-header with-border">
+    <h3 class="box-title">{name}</h3>
+  </div>
+  <div class="box-body">
+    <p>Currently no settings available.</p><br>
+    <i>Author: <code>{author}</code></i><br>
+    <i>Identifier: <code>{identifier}</code></i><br>
+    <i>Version: <code>{version}</code></i>
+  </div>
+</div>
