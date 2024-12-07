ebdfc24b2be01eeec1a5a9c069b22e0fa1ec327a
MrAhmalo <ahmalo456@gmail.com>
2024-12-07 17:38:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+apk add git
+
 NEW_FOLDER="tempfolder"
-REPO_URL="https://github.com/your_username/your_repo.git"
+REPO_URL="https://github.com/MrAhmalo/servertoolspro.git"
 ZIP_NAME="servertoolspro.blueprint"
 
 mkdir -p "$NEW_FOLDER"
-
 git clone "$REPO_URL" "$NEW_FOLDER"
+
+cd "$NEW_FOLDER"
+rm -rf install.sh
+cd ..
 
 zip -r "$ZIP_NAME" "$NEW_FOLDER"
 
