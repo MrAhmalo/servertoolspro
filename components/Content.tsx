7a39fe28fb40eaa303a6ff1b1d3de29b64913adb
MrAhmalo <ahmalo456@gmail.com>
2024-12-07 23:58:47 +0100

    Update Content.tsx

--- components/Content.tsx
+++ components/Content.tsx
@@ -59,29 +59,30 @@ const sendCommand = (action: string) => {
 
 const renderCommandsPage = () => {
   return (
+    <div>
       <div className="flex flex-row">
-          <Banner title="Operators" className="bg-gray-700" icon={<FontAwesomeIcon icon={faTerminal} />}>
-              Execute your preconfigured commands.
-          </Banner>
-          
-          <Button.Text onClick={() => sendCommand('wartungsarbeiten')}>Wartungsarbeiten</Button.Text>
+        <Banner title="Operators" className="bg-gray-700" icon={<FontAwesomeIcon icon={faTerminal} />}>
+          Execute your preconfigured commands.
+        </Banner>
       </div>
+      <Button.Text onClick={() => sendCommand('wartungsarbeiten')}>Wartungsarbeiten</Button.Text>
+    </div>
   );
 };
 
 const renderGamemodePage = () => {
   return (
-      <Banner title="Gamemode" className="bg-gray-700" icon={<FontAwesomeIcon icon={faGamepad} />}>
-          Manage the gamemode of your players.
-      </Banner>
+    <Banner title="Gamemode" className="bg-gray-700" icon={<FontAwesomeIcon icon={faGamepad} />}>
+      Manage the gamemode of your players.
+    </Banner>
   );
 };
 
 const renderPlayersPage = () => {
   return (
-      <Banner title="Players" className="bg-gray-700" icon={<FontAwesomeIcon icon={faUserPlus} />}>
-          Manage the players on your server.
-      </Banner>
+    <Banner title="Players" className="bg-gray-700" icon={<FontAwesomeIcon icon={faUserPlus} />}>
+      Manage the players on your server.
+    </Banner>
   );
 };
 
