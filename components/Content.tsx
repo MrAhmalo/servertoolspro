211949b2aef7e7e930b3f34ff47bffc9d37e8cbe
MrAhmalo <ahmalo456@gmail.com>
2024-12-08 12:17:21 +0100

    did some fixes

--- components/Content.tsx
+++ components/Content.tsx
@@ -3,7 +3,7 @@ import { Dialog } from '@/components/elements/dialog/index';
 import { Input } from '@/components/elements/inputs/index';
 import Label from '@/components/elements/Label';
 import { ServerContext } from '@/state/server';
-import { faGamepad, faPlus, faTerminal, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTerminal } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import Banner from './Banner';
@@ -11,11 +11,110 @@ import Banner from './Banner';
 const Content = () => {
   const [viewing, setViewing] = useState<'commands' | 'gamemode' | 'players'>('commands');
   const { instance } = ServerContext.useStoreState((state) => state.socket);
+  const [showDialog, setShowDialog] = useState(false);
+  const [customCommand, setCustomCommand] = useState('');
+
+  const sendCommand = (action: string) => {
+    let commandToExecute = '';
+    
+    if (action === 'wartungsarbeiten') {
+      commandToExecute = 'tellraw @p ["",{"text":"⚠: ","bold":true,"color":"red"},{"text":"Wartungsarbeiten","bold":true,"color":"yellow"},"\\n",{"text":"Server offline für ca. 15-20 minuten","color":"white"}]';
+    } else {
+      commandToExecute = action;
+    }
+  
+    if (instance) {
+      console.log('Sending command', commandToExecute);
+      instance.send('send command', commandToExecute);
+    }
+  };
+
+  const renderCommandsPage = () => {
+    const openDialog = () => setShowDialog(true);
+    const closeDialog = () => {
+      setCustomCommand('');
+      setShowDialog(false);
+    };
+  
+    const handleCustomCommandSubmit = (e: React.FormEvent) => {
+      e.preventDefault();
+      if (customCommand.trim()) {
+        sendCommand(customCommand, instance);
+        closeDialog();
+      } else {
+        console.warn('Please enter a valid command.');
+      }
+    };
+  
+    const handleCommandKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === 'Enter' && customCommand.trim().length > 0) {
+        e.preventDefault();
+        handleCustomCommandSubmit(e as unknown as React.FormEvent);
+      }
+    };
+    
+  
+    return (
+      <div>
+        <div>
+          <Banner title="Operators" className="bg-gray-700" icon={<FontAwesomeIcon icon={faTerminal} />}>
+            Execute your preconfigured commands.
+          </Banner>
+        </div>
+        <div className="flex flex-row mt-4">
+          <Button.Text className="mr-2" onClick={openDialog}>
+            <FontAwesomeIcon icon={faPlus} />
+          </Button.Text>
+  
+          <Dialog open={showDialog} onClose={closeDialog} title={'Enter new command'}>
+            <form id="custom-command-form" onSubmit={handleCustomCommandSubmit}>
+              <Label>Command</Label>
+              <Input.Text
+                placeholder="give @a minecraft:diamond 1"
+                value={customCommand}
+                onChange={(e) => setCustomCommand(e.target.value)}
+                onKeyDown={handleCommandKeyDown}
+              />
+  
+              <Dialog.Footer>
+                <Button.Text onClick={closeDialog}>Cancel</Button.Text>
+                <Button
+                  disabled={customCommand.trim().length === 0}
+                  type="submit"
+                  form="custom-command-form"
+                >
+                  Command senden
+                </Button>
+              </Dialog.Footer>
+            </form>
+          </Dialog>
+  
+          <Button.Text onClick={() => sendCommand('wartungsarbeiten', instance)}>Wartungsarbeiten</Button.Text>
+        </div>
+      </div>
+    );
+  };
+
+  const renderGamemodePage = () => {
+    return (
+      <Banner title="Gamemode" className="bg-gray-700" icon={<FontAwesomeIcon icon={faGamepad} />}>
+        Manage the gamemode of your players.
+      </Banner>
+    );
+  };
+  
+  const renderPlayersPage = () => {
+    return (
+      <Banner title="Players" className="bg-gray-700" icon={<FontAwesomeIcon icon={faUserPlus} />}>
+        Manage the players on your server.
+      </Banner>
+    );
+  };
 
   const renderPageContent = () => {
     switch (viewing) {
       case 'commands':
-        return renderCommandsPage(instance);
+        return renderCommandsPage(instance, showDialog, setShowDialog, customCommand, setCustomCommand);
       case 'gamemode':
         return renderGamemodePage();
       case 'players':
@@ -46,106 +145,6 @@ const Content = () => {
         {renderPageContent()}
       </div>
     </>
-  );
-};
-
-const sendCommand = (action: string, instance : any) => {
-  let commandToExecute = '';
-  
-  if (action === 'wartungsarbeiten') {
-    commandToExecute = 'tellraw @p ["",{"text":"⚠: ","bold":true,"color":"red"},{"text":"Wartungsarbeiten","bold":true,"color":"yellow"},"\\n",{"text":"Server offline für ca. 15-20 minuten","color":"white"}]';
-  } else {
-    commandToExecute = action;
-  }
-
-  if (instance) {
-    console.log('Sending command', commandToExecute);
-    instance.send('send command', commandToExecute);
-  }
-};
-
-const renderCommandsPage = (instance : any) => {
-  const [showDialog, setShowDialog] = useState(false);
-  const [customCommand, setCustomCommand] = useState('');
-
-  const openDialog = () => setShowDialog(true);
-  const closeDialog = () => {
-    setCustomCommand('');
-    setShowDialog(false);
-  };
-
-  const handleCustomCommandSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (customCommand.trim()) {
-      sendCommand(customCommand, instance);
-      closeDialog();
-    } else {
-      console.warn('Please enter a valid command.');
-    }
-  };
-
-  const handleCommandKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && customCommand.trim().length > 0) {
-      e.preventDefault();
-      handleCustomCommandSubmit(e as unknown as React.FormEvent);
-    }
-  };
-  
-
-  return (
-    <div>
-      <div>
-        <Banner title="Operators" className="bg-gray-700" icon={<FontAwesomeIcon icon={faTerminal} />}>
-          Execute your preconfigured commands.
-        </Banner>
-      </div>
-      <div className="flex flex-row mt-4">
-        <Button.Text className="mr-2" onClick={openDialog}>
-          <FontAwesomeIcon icon={faPlus} />
-        </Button.Text>
-
-        <Dialog open={showDialog} onClose={closeDialog} title={'Enter new command'}>
-          <form id="custom-command-form" onSubmit={handleCustomCommandSubmit}>
-            <Label>Command</Label>
-            <Input.Text
-              placeholder="give @a minecraft:diamond 1"
-              value={customCommand}
-              onChange={(e) => setCustomCommand(e.target.value)}
-              onKeyDown={handleCommandKeyDown}
-            />
-
-            <Dialog.Footer>
-              <Button.Text onClick={closeDialog}>Cancel</Button.Text>
-              <Button
-                disabled={customCommand.trim().length === 0}
-                type="submit"
-                form="custom-command-form"
-              >
-                Command senden
-              </Button>
-            </Dialog.Footer>
-          </form>
-        </Dialog>
-
-        <Button.Text onClick={() => sendCommand('wartungsarbeiten', instance)}>Wartungsarbeiten</Button.Text>
-      </div>
-    </div>
-  );
-};
-
-const renderGamemodePage = () => {
-  return (
-    <Banner title="Gamemode" className="bg-gray-700" icon={<FontAwesomeIcon icon={faGamepad} />}>
-      Manage the gamemode of your players.
-    </Banner>
-  );
-};
-
-const renderPlayersPage = () => {
-  return (
-    <Banner title="Players" className="bg-gray-700" icon={<FontAwesomeIcon icon={faUserPlus} />}>
-      Manage the players on your server.
-    </Banner>
   );
 };
 
