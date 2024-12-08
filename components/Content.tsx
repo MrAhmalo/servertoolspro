12cad3d9b2a7cd74ee6d1cc96c21ec87846675ba
MrAhmalo <ahmalo456@gmail.com>
2024-12-08 15:07:47 +0100

    Update Content.tsx

--- components/Content.tsx
+++ components/Content.tsx
@@ -144,11 +144,11 @@ const Content = () => {
           </Dialog>
   
           <Button.Text onClick={() => restart1Command()} className="mr-2">Restart</Button.Text>
-          <Button.Text onClick={() => maintance1Command()} className="mr-1">Maintance</Button.Text>
-          <Button.Text onClick={() => maintance5Command()} className="mr-1">Maintance (5m)</Button.Text>
-          <Button.Text onClick={() => maintance10Command()} className="mr-1">Maintance (10m)</Button.Text>
-          <Button.Text onClick={() => maintance30Command()} className="mr-1">Maintance (30m)</Button.Text>
-          <Button.Text onClick={() => maintance60Command()} className="mr-1">Maintance (1h)</Button.Text>
+          <Button.Text onClick={() => maintance1Command()} className="mr-2">Maintance</Button.Text>
+          <Button.Text onClick={() => maintance5Command()} className="mr-2">Maintance (5m)</Button.Text>
+          <Button.Text onClick={() => maintance10Command()} className="mr-2">Maintance (10m)</Button.Text>
+          <Button.Text onClick={() => maintance30Command()} className="mr-2">Maintance (30m)</Button.Text>
+          <Button.Text onClick={() => maintance60Command()} className="mr-2">Maintance (1h)</Button.Text>
         </div>
       </div>
     );
